c18d3cacb522492da615e8ac0d66b2e61bf4ea3d
yd <6158924+yuzdnt@users.noreply.github.com>
2022-03-10 22:52:40 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/W3lrwkzhX2Z1OjVKzf5tz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d5101d96067d9ef6.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],"/about":["static/chunks/pages/about-0435eb25bb99b98b.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
