f0b145a2d531a742b4618777b41706a1b2a56225
taqin <abdulmuttaqin456@gmail.com>
2023-04-25 11:34:11 +0700

    Update update_star_count.py

--- update_star_count.py
+++ update_star_count.py
@@ -1,6 +1,6 @@
 import os
 import re
-from github import Github
+from github import Github, GithubException
 
 def update_star_count(readme_contents, repo_info):
     pattern = fr'\(⭐\s*\d+\)\s*\[{repo_info.name}\]'
@@ -18,12 +18,12 @@ def main():
     repo_urls = re.findall(r'\(https://github.com/([\w-]+/[\w-]+)\)', readme_contents)
 
     for repo_path in repo_urls:
-    try:
-        repo_info = gh.get_repo(repo_path)
-        readme_contents = update_star_count(readme_contents, repo_info)
-    except github.GithubException as e:
-        print(f"Error while fetching {repo_path}: {e}")
-        continue
+        try:
+            repo_info = gh.get_repo(repo_path)
+            readme_contents = update_star_count(readme_contents, repo_info)
+        except GithubException as e:
+            print(f"Error while fetching {repo_path}: {e}")
+            continue
 
     if readme_contents != readme.decoded_content.decode("utf-8"):
         repo.update_file(
